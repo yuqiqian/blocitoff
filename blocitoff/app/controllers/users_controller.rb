925d3e81c2f13a38e6b96835d1f532f26dcd540d
yuqiqian <yuqi.qian@outlook.com>
2015-05-18 12:51:26 -0400

    user signup finished

--- blocitoff/app/controllers/users_controller.rb
+++ blocitoff/app/controllers/users_controller.rb
@@ -1,15 +1,6 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to root_url, :notice => "Signed up!"
-    else
-      render "new"
-    end
+  def index
   end
   
   private
