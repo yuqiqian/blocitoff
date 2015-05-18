119d97e1bcbdd661b4cce5dae52814bb75516797
yuqiqian <yuqi.qian@outlook.com>
2015-05-18 10:26:50 -0400

    switch to Devise gem

--- blocitoff/config/routes.rb
+++ blocitoff/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   get 'users/new'
   get 'log_out' => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
